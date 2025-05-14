43b5afc0fef7e2cfc967f99f31f8752ba5571c44
Palomadev <paloma166.pah@gmail.com>
2025-05-14 11:33:25 -0300

    att

---
+++ profile.page.js
@@ -0,0 +1,13 @@
+import { $ } from '@wdio/globals'
+
+
+
+class ProfilePage {
+    
+   async profileName (name) {
+    return await $(`//android.widget.TextView[@text="${name}`)
+   }
+
+}
+
+export default new ProfilePage();
